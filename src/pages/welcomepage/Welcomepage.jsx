7d09eaaa98d4acd5c11f7c7c959f25f004a8cc31
GoodnessOno <ononogbugoodness@gmail.com>
2024-05-11 13:21:41 +0100

    welcomepage finale

--- src/pages/welcomepage/Welcomepage.jsx
+++ src/pages/welcomepage/Welcomepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './welcomepage.css'
 import img from '../../assets/aigenerated image.png'
 import logo from '../../assets/GOODNESS ONONOGBU LOGO.png'
+import { Link } from 'react-router-dom'
 
 const Welcomepage = () => {
   return (
@@ -13,13 +14,17 @@ const Welcomepage = () => {
         <div className='welcomepage-greeting'>
         <h1>Hi, My name is Goodness.</h1>
         <p>I transform designs into <b>SEEMLESS EXPERIENCES</b></p>
+        <br />
         <p>Welcome to my portfolio! <i>I'm a front-end developer</i> with an interest in integrating AI into digital experiences. 
         Here, you'll find examples of my work showcasing intuitive user interfaces and artificial intelligence. 
         <br />
         <br />
         Click on the <b>GET STARTED BUTTON BELOW </b> to experience how design and technology intersect to create seamless online interactions.</p>
+        <br />
         <div className='welcomepage-button'>
-          <button>GET STARTED</button>
+          <Link to="/homepage">
+          <button className='rotate-scale-up scale-up-center'>GET STARTED</button>
+          </Link>
         </div>
         </div>
       <div className='welcomepage-image'>
