4d200651283fdd3a567db4eaa025f51202828694
GoodnessOno <ononogbugoodness@gmail.com>
2024-06-16 08:43:11 +0100

    homepage final commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,34 +8,34 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
    
   return (
-    <div className='footer'>
-      <div className='footer-content'>
+    <main className='footer'>
         <div className='footer-links'>
-      <div className='footer-links-logo'>
-        <img src={logo} alt="logo" />
-      </div>
-        <div className='footer-links-header'>
-        <h2>CONTACT ME</h2>
+          <div className='footer-links-logo'>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className='footer-links-divider'>
+            <div className='footer-links-header'>
+              <h2>CONTACT ME</h2>
+            </div>
+            <div className='footer-links-contact'>
+              <ul>
+                <li>
+                  <a href="mailto:ononogbugoodness@gmail.com" target='_blank' rel='noopener noreferrer'><BiLogoGmail />EMAIL</a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/goodness-ononogbu-aicmc/" target='_blank' rel='noopener noreferrer'><IoLogoLinkedin />LINKED</a>
+                </li>
+                <li>
+                  <a href="https://github.com/GoodnessOno" target='_blank' rel='noopener noreferrer'><IoLogoGithub />GITHUB</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className='footer-links-contact'>
-        <ul>
-            <li>
-            <a href="ononogbugoodness@gmail.com" target='_blank' rel='noopener noreferrer'><BiLogoGmail />EMAIL</a>
-            </li>
-            <li>
-            <a href="https://www.linkedin.com/in/goodness-ononogbu-aicmc/" target='_blank' rel='noopener noreferrer'><IoLogoLinkedin />LINKED</a>
-            </li>
-            <li>
-            <a href="https://github.com/GoodnessOno" target='_blank' rel='noopener noreferrer'><IoLogoGithub />GITHUB</a>
-            </li>
-          </ul>
-        </div>
-        </div>
-        <div className="footer">
+        <div className="footer-date">
           <p><b>&copy; {currentYear} GOODNESS ONONOGBU. All rights reserved.</b></p>
         </div>
-        </div>
-      </div>
+    </main>
   )
 }
 
