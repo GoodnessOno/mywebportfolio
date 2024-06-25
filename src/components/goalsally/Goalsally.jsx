168f75ef121f3c7d56b028a962f6eab1fc4cc5de
GoodnessOno <ononogbugoodness@gmail.com>
2024-06-25 13:51:02 +0100

    goals ally correction

--- src/components/goalsally/Goalsally.jsx
+++ src/components/goalsally/Goalsally.jsx
@@ -16,25 +16,23 @@ const Goalsally = () => {
         <h2>GoalsAlly</h2>
         <h3>Join Forces, Succeed</h3>
         <p>
-            IntelliUX is a cutting-edge solution designed to transform the way users interact with your website.
+            GoalsAlly is a software application designed to help organizations, countries and individuals make more contributions, gain more support and track their progress towards sustainability goals. 
             <br />
-            It leverages advanced artificial intelligence and machine learning algorithms to meticulously track user activities on your website. 
-            By analyzing user behavior in real-time, the tool provides actionable feedback and personalized improvement suggestions tailored to enhance the overall user experience.
+            It leverages advanced artificial intelligence and machine learning algorithms to track and provide reminders to the un sdgs. 
             <br />
             <br />
             <b>Key Features - </b>
-            <li> 1. Real-Time Activity Tracking</li>
-           <li> 2. Intelligent Feedback</li>
-            <li> 3. Personalized Improvement Suggestions</li>
-            <li> 4. Continuous Learning</li>
+            <li> 1. User verification/authentication</li>
+            <li> 2. Goal tracker</li>
+            <li> 3. Project Collaborations</li>
             <br />
             <br />
-            The idea behind IntelliUX is born out of the need to aid developers put software users first, though still in its development stage, you can click on the link below to view the github repository.
+            The idea behind GoalsAlly is a social media application and virtual assistant all for the purpose of the achievement of united nations sustainability development goals.
             Or, if you desire to know more about this software, kindly click on this link to view our live google slides.
             <br />
             <br />
             <li><a href="https://tinyurl.com/goalsally-slides" target="_blank" rel="noopener noreferrer">GoalsAlly Software</a></li>
-            <li><a href="https://github.com/GOALSALLY/GoalsAlly" target="_blank" rel="noopener noreferrer"><IoLogoGithub />GoalsAlly Github Repository</a></li>
+            <li><a href="https://github.com/GoodnessOno/goalsallysdgapp" target="_blank" rel="noopener noreferrer"><IoLogoGithub />GoalsAlly Github Repository</a></li>
         </p>
         <h3>Technologies used - Javascript, ReactJS, Python, HTML, CSS, ExpressJS</h3>
       </div>
